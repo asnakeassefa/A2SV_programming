1ac3f61c75f140243a8647535877c109f9751c1c
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2022-12-29 19:14:46 +0300

    Time: 37 ms (91.05%), Space: 13.8 MB (99.54%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,18 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        p1,p2,p3 = m-1,n-1,(len(nums1)-1)
+        ptr1 = m-1
+        ptr2 = n-1
+        ptr3 = (len(nums1) - 1)
         
-        while p2 >= 0:
-            if p1 == -1:
-                nums1[p3] = nums2[p2]
-                p2 -= 1
-                p3 -= 1
-            elif nums1[p1] < nums2[p2]:
-                nums1[p3] = nums2[p2]
-                p3 -= 1
-                p2 -= 1
+        while ptr2 >= 0:
+            if ptr1 == -1:
+                nums1[ptr3] = nums2[ptr2]
+                ptr2 -= 1
+                ptr3 -= 1
+            elif nums1[ptr1] < nums2[ptr2]:
+                nums1[ptr3] = nums2[ptr2]
+                ptr3 -= 1
+                ptr2 -= 1
             else:
-                nums1[p3] = nums1[p1]
-                p3 -= 1
-                p1 -= 1
+                nums1[ptr3] = nums1[ptr1]
+                ptr3 -= 1
+                ptr1 -= 1
