b35a3bf46454c5bdc5fd9508471a064389958df9
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2022-12-29 20:01:52 +0300

    Time: 1189 ms (9.26%), Space: 14 MB (72.18%) - LeetHub

---
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    #define a function which compair two words
+    def compair(self,word1,word2):
+        set1 = set()
+        set2 = set()
+        for word in word1:
+            set1.add(word)
+        for word in word2:
+            set2.add(word)
+        if set1 == set2:
+            return True
+    def similarPairs(self, words: List[str]) -> int:
+        ans = 0
+        for index,word in enumerate(words):
+            for val in words[index+1:]:
+                if self.compair(word,val):
+                    ans += 1
+        return ans
