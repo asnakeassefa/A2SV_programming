59a7ba75a8bf4a2ec46152fe6b0797d17b6274a3
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2023-01-04 16:08:31 +0300

    Time: 61 ms (63.56%), Space: 14 MB (9.52%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,4 +1,13 @@
 class Solution:
-    def multiply(self, num1: str, num2: str) -> str:
-        ans = int(num1) * int(num2)
-        return str(ans)
+	def convertToInt(self,number):
+		count = 1
+		final = 0
+		nums = number[::-1]
+		for num in nums:
+			final += (ord(num) - ord('0')) * count
+			count *= 10
+		return final
+    
+	def multiply(self, num1: str, num2: str) -> str:
+		ans = self.convertToInt(num1) * self.convertToInt(num2)
+		return str(ans)
