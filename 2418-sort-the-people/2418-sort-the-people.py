f12b1b785d3ff8e301fbf04314785453c15e6164
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2023-01-17 18:58:07 +0300

    Time: 591 ms (10.42%), Space: 14.4 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -3,10 +3,12 @@ class Solution:
         
         length = len(names)
         for i in range(length):
-            for j in range(length-1-i):
-                if heights[j] < heights[j+1]:
-                    heights[j],heights[j+1] = heights[j+1],heights[j]
-                    names[j],names[j+1] = names[j+1],names[j]
+            minindex = i
+            for j in range(i+1,length):
+                if heights[j] > heights[minindex]:
+                    minindex = j
+            heights[minindex],heights[i] = heights[i],heights[minindex]
+            names[minindex],names[i] = names[i],names[minindex]
         
         return names
         
