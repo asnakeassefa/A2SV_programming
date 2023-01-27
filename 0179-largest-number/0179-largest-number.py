3b2a91e9325ada95d434925967739e53a84fb294
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2023-01-27 16:51:38 +0300

    Time: 72 ms (35.26%), Space: 13.9 MB (61.60%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,11 +1,17 @@
+class compair(str):
+    def __lt__(x,y):
+        return x+y > y+x
+
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         length = len(nums)
+        nums[:] = map(str,nums)
         
-        for i in range(length):
-            for j in range(length-1-i):
-                if str(nums[j]) + str(nums[j+1]) < str(nums[j+1]) + str(nums[j]):
-                    nums[j], nums[j+1] = nums[j+1],nums[j]
+        # for i in range(length):
+        #     for j in range(length-1-i):
+        #         if str(nums[j]) + str(nums[j+1]) < str(nums[j+1]) + str(nums[j]):
+        #             nums[j], nums[j+1] = nums[j+1],nums[j]
+        nums.sort(key=compair)
         ans = ""
         
         for i in range(length):
