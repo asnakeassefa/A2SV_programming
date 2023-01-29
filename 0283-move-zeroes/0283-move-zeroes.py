7112e7e19e5cf0a9b2dd450da00220780bbeacb0
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2023-01-29 17:03:44 +0300

    Time: 188 ms (53.12%), Space: 15.7 MB (12.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,12 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = 0
-        index = [] * nums.count(0)
-        for x in range(len(nums)):
-            if nums[x] == 0:
-                index.append(x)
-        
-        for x in range(len(index)):
-            nums.append(nums.pop(index[x] - n))
-            n += 1
+        length = len(nums)
+        holder = 0
+        runner = 0
+        while runner < length:
+            while holder < length and nums[holder] != 0:
+                holder += 1
+            while runner < length and nums[runner] == 0:
+                runner += 1
+            if runner < length and holder < length and holder < runner:
+                nums[holder],nums[runner] = nums[runner],nums[holder]
+                holder += 1
+            runner += 1
