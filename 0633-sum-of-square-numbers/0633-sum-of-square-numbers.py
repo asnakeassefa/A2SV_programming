61b245f5935e55753f57e75aa7facca5f36b0956
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2023-01-30 19:05:37 +0300

    Time: 823 ms (14.25%), Space: 13.8 MB (57.22%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sqroot(self,num):
+        temp = int(math.sqrt(num))
+        return temp ** 2 == num
+
+    def judgeSquareSum(self, c: int) -> bool:
+        length = int(math.sqrt(c) + 1)
+        for num in range(length):
+            num2 = c-(num ** 2)
+            if self.sqroot(num ** 2) and self.sqroot(num2):
+                return True
+        return False
