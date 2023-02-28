e789e199896d8538a344007f90aedd8c57a1f61d
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2023-02-28 11:19:08 +0300

    Time: 32 ms (73.45%), Space: 13.7 MB (99.90%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,14 +1,14 @@
 class Solution:
-    def recu(self,n,Dict):
+    def memo(self,n,Dict):
         if n == 1: return 1
         if n == 0: return 0
         
         if Dict.get(n):
             return Dict[n]
         
-        ans = self.recu(n-1,Dict) + self.recu(n-2,Dict)
+        ans = self.memo(n-1,Dict) + self.memo(n-2,Dict)
         Dict[n] = ans
         return ans
     def fib(self, n: int) -> int:
         Dict = dict()
-        return self.recu(n,Dict)
+        return self.memo(n,Dict)
