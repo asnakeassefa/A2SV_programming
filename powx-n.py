a708d5b931434e37f5fc7e8090178079593a3e92
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2023-03-02 22:12:52 +0300

    added powx-n.python

---
+++ powx-n.py
@@ -0,0 +1,22 @@
+class Solution:
+    
+    def myPow(self, x: float, n: int) -> float:
+        
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        if n > 0:
+            if n % 2 != 0:
+                ans = self.myPow(x,(n-1)//2)
+                return x * ans * ans
+            else:
+                ans = self.myPow(x,n//2)
+                return ans * ans
+        if n < 0:
+            if n % 2 != 0:
+                ans = self.myPow(1/x,-(n-1)//2)
+                return x * ans * ans
+            else:
+                ans = self.myPow(1/x,-n//2)
+                return ans * ans
