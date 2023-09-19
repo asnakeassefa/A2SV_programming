17b8bb588dd30e3ed0b51294e75447b59dedf3ac
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2023-09-19 10:29:09 +0300

    added 2-keys-keyboard.python

--- 2-keys-keyboard.py
+++ 2-keys-keyboard.py
@@ -1,14 +1,19 @@
 class Solution:
+    def dp(self,curr,n,memo,ans,copied):
+        if curr[0] > n:
+            return float('inf')
+        if curr[0] == n:
+            return ans
+        if curr in memo:
+            return memo[curr]
+        if copied:
+            memo[curr] = self.dp((curr[0]+curr[1],curr[1]),n,memo,ans+1,False)
+        else:
+            memo[curr] = min(self.dp((curr[0],curr[0]),n,memo,ans+1,True), self.dp((curr[0]+curr[1],curr[1]),n,memo,ans+1,False))
+
+        return memo[curr]
     def minSteps(self, n: int) -> int:
         if n ==1:
             return 0
-        d = 2
-        ans = 0
-        while d * d <= n:
-            while n % d == 0:
-                n //= d
-                ans += d
-            d += 1
-        if n > 1:
-            ans += n
-        return ans
+        memo = defaultdict(int)
+        return self.dp((1,1),n,memo,1,True)
