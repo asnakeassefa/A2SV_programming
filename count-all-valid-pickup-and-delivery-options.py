5404038bae02eb7a500989b198af6967bee53c14
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2023-10-12 21:43:52 +0300

    added count-all-valid-pickup-and-delivery-options.python

--- count-all-valid-pickup-and-delivery-options.py
+++ count-all-valid-pickup-and-delivery-options.py
@@ -1,16 +1,5 @@
 class Solution:
     def countOrders(self, n: int) -> int:
-        def dp(val,memo):
-            # print(val)
-            if val[0] < 0:
-                return 0
-            if val == (0,0):
-                return 1
-            if val in memo:
-                return memo[val]
-            memo[val] = dp((val[0]-1,val[1]),memo) * val[0]
-            if val[0] < val[1]:
-                memo[val] += dp((val[0],val[1]-1),memo) * (val[1] - val[0])
-            # print(memo[val])
-            return memo[val]
-        return dp((n,n),{}) %( 10 ** 9 + 7)
+        if n == 1:
+            return 1
+        return (n*(((2*n)-1)* self.countOrders(n-1))) % (10 ** 9 + 7)
